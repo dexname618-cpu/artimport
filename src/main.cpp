f72458dbde2523df69d91703fa0cf426d5696d90
dexname618-cpu <dexname618@gmail.com>
2026-05-21 13:37:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,41 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/EditorUI.hpp>
-#include <Geode/modify/LevelEditorLayer.hpp>
-#include <Geode/binding/EditorUI.hpp>
-#include <Geode/binding/LevelEditorLayer.hpp>
 #include <Geode/binding/GameObject.hpp>
 #include <Geode/binding/FLAlertLayer.hpp>
+#include <Geode/binding/LevelEditorLayer.hpp>
 #include <filesystem>
+
 using namespace geode::prelude;
 
-#define IMAGE_PATH "/storage/emulated/0/GDPictures/img.png"
-
-CCTexture2D* getCustomTexture() {
-    std::string path = IMAGE_PATH;
-
-    if (!std::filesystem::exists(path)) return nullptr;
-
-    CCTextureCache::get()->removeTextureForKey(path.c_str());
-    auto texture = CCTextureCache::get()->addImage(path.c_str(), false);
-
-    if (texture) {
-        texture->setAntiAliasTexParameters();
+static CCTexture2D* getCustomTexture() {
+    std::string path = "/storage/emulated/0/GDPictures/img.png";
+    if (!std::filesystem::exists(path)) {
+        return nullptr;
     }
-
-    return texture;
+    CCTextureCache::get()->removeTextureForKey(path.c_str());
+    auto tex = CCTextureCache::get()->addImage(path.c_str(), false);
+    if (tex) {
+        tex->setAntiAliasTexParameters();
+    }
+    return tex;
 }
 
 struct MyEditorUI : geode::Modify<MyEditorUI, EditorUI> {
+
     bool init(LevelEditorLayer* lel) {
-        if (!EditorUI::init(lel)) return false;
+        if (!EditorUI::init(lel)) {
+            return false;
+        }
 
         auto editMenu = this->getChildByID("edit-tab-menu");
-        if (!editMenu) return true;
+        if (!editMenu) {
+            return true;
+        }
 
         auto label = CCLabelBMFont::create("IMG", "bigFont.fnt");
         label->setScale(0.4f);
 
-        auto bg = CCScale9Sprite::create(
-            "GJ_button_01.png", {0, 0, 40, 40}
-        );
+        auto bg = CCScale9Sprite::create("GJ_button_01.png", {0, 0, 40, 40});
         bg->setContentSize({40, 40});
         label->setPosition({20, 20});
         bg->addChild(label);
@@ -54,53 +52,45 @@ struct MyEditorUI : geode::Modify<MyEditorUI, EditorUI> {
         return true;
     }
 
-    void onImportImage(CCObject*) {
-        auto texture = getCustomTexture();
+    void onImportImage(CCObject* sender) {
+        auto tex = getCustomTexture();
 
-        if (!texture) {
+        if (!tex) {
             FLAlertLayer::create(
                 "Помилка",
-                "Файл не знайдено!\nПоклади <cy>img.png</cy> в\n<cg>/storage/emulated/0/GDPictures/</cg>",
+                "Поклади img.png в /storage/emulated/0/GDPictures/",
                 "OK"
             )->show();
             return;
         }
 
         auto selected = this->getSelectedObjects();
-
         if (!selected || selected->count() == 0) {
             FLAlertLayer::create(
                 "Підказка",
-                "Спочатку <cy>вибери блок</cy> в редакторі,\nпотім натисни IMG!",
+                "Вибери блок в редакторі потім натисни IMG!",
                 "OK"
             )->show();
             return;
         }
 
-        for (int i = 0; i < selected->count(); i++) {
-            auto obj = typeinfo_cast<GameObject*>(
-                selected->objectAtIndex(i)
-            );
-            if (!obj) continue;
-
-            float scaleX = obj->getScaleX();
-            float scaleY = obj->getScaleY();
-
-            auto mainSprite = typeinfo_cast<CCSprite*>(
-                obj->getChildByTag(0)
-            );
-            if (!mainSprite) {
-                mainSprite = typeinfo_cast<CCSprite*>(obj);
+        for (unsigned int i = 0; i < selected->count(); i++) {
+            auto obj = typeinfo_cast<GameObject*>(selected->objectAtIndex(i));
+            if (!obj) {
+                continue;
             }
 
-            if (mainSprite) {
-                mainSprite->setTexture(texture);
+            auto spr = typeinfo_cast<CCSprite*>(obj->getChildByTag(0));
+            if (!spr) {
+                spr = typeinfo_cast<CCSprite*>(obj);
+            }
 
-                float texW = texture->getPixelsWide() / scaleX;
-                float texH = texture->getPixelsHigh() / scaleY;
-
-                mainSprite->setTextureRect({0, 0, texW, texH});
+            if (spr) {
+                spr->setTexture(tex);
+                float w = (float)tex->getPixelsWide();
+                float h = (float)tex->getPixelsHigh();
+                spr->setTextureRect({0, 0, w, h});
             }
         }
     }
-}
+};
